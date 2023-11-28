0017f6ab5efcd9f0a122d4fbfae0e1b048de7e48
bernardoprates <bprates.ti@gmail.com>
2023-11-28 19:42:39 -0300

    Aula 28/11 - Inicio

---
+++ src/app/eventos/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import Layout from "@/components/eventos/layout";
+import Tabela from "@/components/eventos/tabela";
+import Evento from "@/core/Evento";
+
+export default function Eventos() {
+    const eventos = Evento.geraEventosMock()
+
+    function eventoSelecionado(evento: Evento) {
+        console.log("Selecionado:" + evento.nome)
+    }
+
+    function eventoExcluido(evento: Evento) {
+        console.log("Excluir:" + evento.nome)
+    }
+
+    return (
+        <div className={`
+            flex justify-center items-center h-screen
+            bg-gradient-to-bl from-indigo-900 
+            via-indigo-400 to-indigo-800
+            text-white
+        `}>
+            <Layout titulo="Cadastro de eventos">
+                <Tabela eventos={eventos}
+                eventoSelecionado={eventoSelecionado}
+                eventoExcluido={eventoExcluido}></Tabela>
+            </Layout>
+        </div>
+    )
+}
